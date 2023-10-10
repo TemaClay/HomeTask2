990a4bd206f7fc6d26a66a28fc71b0b00ab496ff
Artem <koptelovu@gmail.com>
2023-10-11 00:36:13 +0300

    ThirdDigit function added

--- Program.cs
+++ Program.cs
@@ -2,3 +2,12 @@
 {
     return number / 10 % 10;
 }
+
+string ThirdDigit(int number)
+{
+    if (number / 100 == 0)
+        return Convert.ToString(number / 100 % 10);
+    else
+        return "третьей цифры нет";
+}
+
